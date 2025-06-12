7e7857bcce2c9ad53061f30ff3cc7bfdd7a73a92
DSFRN <franconaeldisantis@gmail.com>
2025-06-12 19:54:00 -0300

    Test code for gettin nodename and host

---
+++ src/test.erl
@@ -0,0 +1,18 @@
+-module(test).
+-export([main/0]).
+ 
+main() ->
+  [ID, Host] = string:split(io_lib:format("~s",[node()]), "@"),
+  case ID of
+    "nonode" ->
+      io:format("ERROR: No ID was especified.~n");
+    _node_id ->
+      io:format("~p~n", [ID]),
+      case inet:getaddr(Host, inet) of
+        {ok, Addr} ->
+          io:format("~p~n", [Addr]);
+        {error, _Reason} ->
+          io:format("ADDRESS ERROR~n")
+      end
+  end,
+  ok.
